259ba6b7dc01a0e10aa7672dd7b278d17f79c5b7
Al3-001 <alejandrox001@outlook.com>
2024-12-12 15:00:06 -0800

    changed searh hostory location to create car tables

--- sql/CREATE_Car_tables.sql
+++ sql/CREATE_Car_tables.sql
@@ -98,6 +98,15 @@ create table if not exists Book(
     foreign key (car_ID) references Car_Storage (car_ID) ON UPDATE cascade
 );
 
+CREATE TABLE IF NOT EXISTS Search_History (
+    search_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(10), 
+    branch_location VARCHAR(50),
+    search_date DATE,
+    car_id VARCHAR(10),
+    search_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
 
 
